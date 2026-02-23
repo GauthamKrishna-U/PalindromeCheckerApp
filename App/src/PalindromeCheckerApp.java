168e33becccaa4b3e3cc050d2f1dbd2d8ef030b9
GauthamKrishna-U <gauthamkrishna1506@gmail.com>
2026-02-23 15:35:46 +0530

    GauthamKrishna-U Added UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,9 +1,24 @@
-// version 2.8
-//author Abc
-//useCase 2:
-public class PalindroneCheckerApp {
+public class UseCase2PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("Modify this logic to usecase 2");
+
+        String input = "madam";
+
+        System.out.println("Input text: " + input);
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Is it a Palindrome? true");
+        } else {
+            System.out.println("Is it a Palindrome? false");
+        }
     }
 }
