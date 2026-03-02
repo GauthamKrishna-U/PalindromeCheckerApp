04171d45ed7bf2c606efeec3abb4d24636d4c840
GauthamKrishna-U <gk0022@srmist.edu.in>
2026-03-02 15:06:45 +0530

    [GauthamKrishna-U] Added >> [UC7]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,31 +2,35 @@
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "civic";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
 
-        for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
-        }
+                String input = "refer";
 
-        boolean isPalindrome = true;
+                Deque<Character> deque = new ArrayDeque<>();
 
-        while (!queue.isEmpty()) {
-            if (!queue.poll().equals(stack.pop())) {
-                isPalindrome = false;
-                break;
+                for (char c : input.toCharArray()) {
+                    deque.addLast(c);
+                }
+
+                boolean isPalindrome = true;
+
+                while (deque.size() > 1) {
+                    char front = deque.removeFirst();
+                    char rear = deque.removeLast();
+
+                    if (front != rear) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                System.out.println("Input: " + input);
+                System.out.println("Is Palindrome?: " + isPalindrome);
             }
         }
-
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome?: " + isPalindrome);
-    }
-}
-```
