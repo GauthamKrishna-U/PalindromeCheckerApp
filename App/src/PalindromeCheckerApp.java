f7a465dd0beced7a966f544af57e8c35ae0fce19
GauthamKrishna-U <gk0022@srmist.edu.in>
2026-03-02 15:15:57 +0530

    [GauthamKrishna-U] Added >> [UC9]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -9,28 +9,24 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+                String input = "madam";
 
-                String input = "level";
-
-                LinkedList<Character> list = new LinkedList<>();
-
-                for (char c : input.toCharArray()) {
-                    list.addLast(c);
-                }
-
-                boolean isPalindrome = true;
-
-                while (list.size() > 1) {
-                    char front = list.removeFirst();
-                    char rear = list.removeLast();
-
-                    if (front != rear) {
-                        isPalindrome = false;
-                        break;
-                    }
-                }
+                boolean isPalindrome = check(input, 0, input.length() - 1);
 
                 System.out.println("Input: " + input);
                 System.out.println("Is Palindrome?: " + isPalindrome);
+            }
+
+            private static boolean check(String s, int start, int end) {
+
+                if (start >= end) {
+                    return true;
+                }
+
+                if (s.charAt(start) != s.charAt(end)) {
+                    return false;
+                }
+
+                return check(s, start + 1, end - 1);
             }
         }
